dd31a306208707c90e09e649c6c29633ad296194
kingachwaleba <kingach6@gmail.com>
2021-05-12 15:18:17 +0200

    Created inner class

--- app/src/main/java/com/example/backgroundtasks/MainActivity.java
+++ app/src/main/java/com/example/backgroundtasks/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.backgroundtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class TaskGetInfo extends AsyncTask<String, Void, FileInfo> {
+
+        @Override
+        protected FileInfo doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(FileInfo fileInfo) {
+            super.onPostExecute(fileInfo);
+        }
     }
 }
