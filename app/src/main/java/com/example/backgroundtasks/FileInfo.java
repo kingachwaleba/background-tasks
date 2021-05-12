cb15a9cd0ac04ebb7ef5fae8ad950a5c641e79a7
kingachwaleba <kingach6@gmail.com>
2021-05-12 15:11:09 +0200

    Added fields, getters and setters

--- app/src/main/java/com/example/backgroundtasks/FileInfo.java
+++ app/src/main/java/com/example/backgroundtasks/FileInfo.java
@@ -1,4 +1,23 @@
 package com.example.backgroundtasks;
 
 public class FileInfo {
+
+    int fileSize;
+    String fileType;
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
 }
