a218033264be377b5fd5d72d9f521f528c3d776a
kingachwaleba <kingach6@gmail.com>
2021-05-30 20:58:40 +0200

    Added reference to components

--- app/src/main/java/com/example/backgroundtasks/MainActivity.java
+++ app/src/main/java/com/example/backgroundtasks/MainActivity.java
@@ -4,13 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText urlAddress;
+    private TextView fileSize;
+    private TextView fileType;
+    private TextView downloadedB;
+    private Button getInfoButton;
+    private Button downloadFileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlAddress = findViewById(R.id.inputURLaddress);
+
+        fileSize = findViewById(R.id.fileSizeValue);
+        fileType = findViewById(R.id.fileTypeValue);
+        downloadedB = findViewById(R.id.downloadedBvalue);
+
+        getInfoButton = findViewById(R.id.getInfoButton);
+        downloadFileButton = findViewById(R.id.downloadFileButton);
     }
 
     public class TaskGetInfo extends AsyncTask<String, Void, FileInfo> {
