c500f0a47b443573577695fd83edd0d54f55548c
kingachwaleba <kingach6@gmail.com>
2021-05-31 21:21:50 +0200

    Created class fields

--- app/src/main/java/com/example/backgroundtasks/MyIntentService.java
+++ app/src/main/java/com/example/backgroundtasks/MyIntentService.java
@@ -1,12 +1,18 @@
 package com.example.backgroundtasks;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.content.Intent;
 
 import androidx.annotation.Nullable;
 
 public class MyIntentService extends IntentService {
-    
+
+    private static final String ACTION_EXERCISE1 = "com.example.intent_service.action.exercise1";
+    private static final String PARAMETER1 = "com.example.intent_service.extra.parameter1";
+    private static final int NOTIFICATION_ID = 1;
+    private NotificationManager notificationManager;
+
     public MyIntentService(String name) {
         super(name);
     }
