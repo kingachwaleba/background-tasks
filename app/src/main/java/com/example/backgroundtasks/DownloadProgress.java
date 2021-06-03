4ea233e557176b9a989e15a87886dbb91cef7e43
kingachwaleba <kingach6@gmail.com>
2021-06-03 23:41:11 +0200

    Implemented the Parcelable interface

--- app/src/main/java/com/example/backgroundtasks/DownloadProgress.java
+++ app/src/main/java/com/example/backgroundtasks/DownloadProgress.java
@@ -1,4 +1,30 @@
 package com.example.backgroundtasks;
 
-public class DownloadProgress {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class DownloadProgress implements Parcelable {
+    public DownloadProgress(Parcel in) {
+    }
+
+    public static final Creator<DownloadProgress> CREATOR = new Creator<DownloadProgress>() {
+        @Override
+        public DownloadProgress createFromParcel(Parcel in) {
+            return new DownloadProgress(in);
+        }
+
+        @Override
+        public DownloadProgress[] newArray(int size) {
+            return new DownloadProgress[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
 }
