191c31a9e1b22656ea6630c134a5a075078c3c00
kingachwaleba <kingach6@gmail.com>
2021-06-04 12:30:46 +0200

    Overrided methods

--- app/src/main/java/com/example/backgroundtasks/MainActivity.java
+++ app/src/main/java/com/example/backgroundtasks/MainActivity.java
@@ -42,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
